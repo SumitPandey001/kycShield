ba4ff53ffb74b1e5be4b7483566a87647303fbd3
SumitPandey001 <126563873+SumitPandey001@users.noreply.github.com>
2023-07-13 13:22:43 +0530

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,10 +6,10 @@ export default function Footer() {
     <div
     className='h-40 custom-bg p-5'>
       <div className='text-white text-bold text-center m-5'>
-        This is project done by Lalit and Gaurav as part of Minor Project for MSc Computer Science, Department of Computer Science, University of Delhi  
+        This is project done by Lalit , Santosh and Sumit.  
       </div>
       <div className='text-white text-bold text-center'>
-        For more details goto <a className="text-red-400 font-mono hover:underline p-1" href='https://www.github.com/khulalit'>Github</a>
+        For more details goto <a className="text-red-400 font-mono hover:underline p-1" href='https://github.com/SumitPandey001'>Github</a>
       </div>
     </div>
   )
