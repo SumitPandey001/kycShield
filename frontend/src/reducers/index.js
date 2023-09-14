8057ac7f2bf8bd0f059199644f26beefe164b130
SumitPandey001 <126563873+SumitPandey001@users.noreply.github.com>
2023-09-14 19:45:42 +0530

    Delete frontend directory

--- frontend/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import addmenuReducer from "./addmenu";
-import connectReducer from "./connect"
-import contractReducer from "./connect";
-import { combineReducers } from "redux";
-
-export const allReducers = combineReducers({
-    open : addmenuReducer,
-    contract : connectReducer
-})
